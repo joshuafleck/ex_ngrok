840da334f945ce955282ba084e68b5d69603955a
Joshua Fleck <joshua.fleck@simplybusiness.co.uk>
2016-11-09 17:30:30 +0000

    project skeleton

---
+++ lib/ex_ngrok.ex
@@ -0,0 +1,20 @@
+defmodule Ngrok do
+  use Application
+
+  # See http://elixir-lang.org/docs/stable/elixir/Application.html
+  # for more information on OTP Applications
+  def start(_type, _args) do
+    import Supervisor.Spec, warn: false
+
+    # Define workers and child supervisors to be supervised
+    children = [
+      # Starts a worker by calling: Ngrok.Worker.start_link(arg1, arg2, arg3)
+      # worker(Ngrok.Worker, [arg1, arg2, arg3]),
+    ]
+
+    # See http://elixir-lang.org/docs/stable/elixir/Supervisor.html
+    # for other strategies and supported options
+    opts = [strategy: :one_for_one, name: Ngrok.Supervisor]
+    Supervisor.start_link(children, opts)
+  end
+end
---
+++ test/ex_ngrok_test.exs
@@ -0,0 +1,8 @@
+defmodule NgrokTest do
+  use ExUnit.Case
+  doctest Ngrok
+
+  test "the truth" do
+    assert 1 + 1 == 2
+  end
+end
